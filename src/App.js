e6129c3f2deec2be85b09fcad7384a3c7e27a4ca
beto <betotrezzini594@gmail.com>
2022-09-06 10:09:05 +0200

    se actualizo main y app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 		<>
 			<Header />
 			<Main />
-			{/* <ItemListContainer /> */}
+			<ItemListContainer />
 			<Footer />
 		</>
 	);
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,11 +5,11 @@ const Main = () => {
 	return (
 		<main className="main">
 			<h1>Listado de Productos</h1>
-			{/* <h2 className="main_title">Tienda de Productos</h2>
+			<h2 className="main_title">Tienda de Productos</h2>
 			<p className="main_parrafo">
 				Te presentamos nuestros producto... A ELEGIR!!!
-			</p> */}
-			{/* <Counter initial={1} stock={10} sinStock={0} /> */}
+			</p>
+
 			<ItemDetailContainer />
 		</main>
 	);
